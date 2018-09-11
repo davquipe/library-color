c9e1516b467b8e1eaac6c65a89cc656afeb7c2ce
davquipe <davidcondori99@gmail.com>
2018-09-11 16:32:02 -0500

    !

---
+++ app/src/main/java/pe/edu/upeu/upeu/ReporteFragment.java
@@ -0,0 +1,20 @@
+package pe.edu.upeu.upeu;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class ReporteFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reporte, container, false);
+    }
+}
